4753e094f35ab1c1db17a1c2d14c10d8bc0fe24e
Prashant <aoiokido123@gmail.com>
2026-05-21 21:27:36 +0545

    update

---
+++ Server/models/offerModel.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+
+const offerSchema = new mongoose,Schema(
+    {
+        title:{
+            type:String,
+            required:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        price:{
+            type:Number,
+            required:true
+        },
+        vehicleType:{
+            type:String,
+            enum:["car","bike","Others"],
+            required:true
+        },
+            duration:{
+                required:String 
+        },
+        isActive:{
+            type:Boolean,
+            required:true
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+exports.module= mongoose.model('Offer',offerSchema)
